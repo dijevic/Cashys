e956821392968a1a2e2de16b880ac1b7201a5975
[diego] <[dielmac7@gmail.com]>
2022-04-01 13:14:06 -0300

    setting up clever cloud db

--- DB/db.config.js
+++ DB/db.config.js
@@ -3,19 +3,14 @@ const { Sequelize } = require('sequelize')
 
 const dbconfig = {}
 
-if (process.env.npm_lifecycle_event == 'dev') {
-    dbconfig.db = 'cashy';
-    dbconfig.userDB = 'root';
-    dbconfig.passwordDB = null
-    dbconfig.hostDB = 'localhost'
-} else {
-    dbconfig.db = process.env.DBNAME
-    dbconfig.userDB = process.env.DBUSERNAME
-    dbconfig.passwordDB = process.env.DBPASSWORD
-    dbconfig.hostDB = process.env.DBHOST
-}
+dbconfig.db = process.env.DBNAME
+dbconfig.userDB = process.env.DBUSERNAME
+dbconfig.passwordDB = process.env.DBPASSWORD
+dbconfig.hostDB = process.env.DBHOST
+
 
 const { db, userDB, passwordDB, hostDB } = dbconfig
+
 
 const sequelize = new Sequelize(db, userDB, passwordDB, {
     host: hostDB,
